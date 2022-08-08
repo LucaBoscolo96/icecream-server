6afd3ac1c0b0fdbe4bc1ce6c1a262d15f6e7a763
l.boscolo.96@gmail.com <l.boscolo.96@gmail.com>
2022-08-08 17:05:39 +0200

    try with remote sql

--- config/Database.js
+++ config/Database.js
@@ -1,10 +1,8 @@
 import { Sequelize } from "sequelize";
 
-const dbConfig = require(config.js);
-
-const db = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
-  host: dbConfig.HOST,
-  dialect: dbConfig.dialect,
+const db = new Sequelize("NRhzKltyrs", "NRhzKltyrs", "j40aRN8cJP", {
+  host: "remotemysql.com",
+  dialect: "mysql",
 });
 
 export default db;
