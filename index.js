fc3c20f1e0dee3ee91529af05941771fa8cdb2a0
l.boscolo.96@gmail.com <l.boscolo.96@gmail.com>
2022-08-10 22:02:21 +0200

    hi

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.use(
 app.use(
   cors({
     credentials: true,
-    origin: "https://main--venerable-jelly-c59b68.netlify.app/",
+    origin: "https://main--venerable-jelly-c59b68.netlify.app",
   })
 );
 app.use(express.json());
